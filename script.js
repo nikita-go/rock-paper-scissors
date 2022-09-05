a81a73234e7530c89b76ee9d93f3db9fbb438e1b
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-09-05 10:21:50 +0800

    Added UI

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
+let computerScore = 0,
+    playerScore = 0;
+
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ['Rock', 'Paper', 'Scissors'];
     choice = choices[Math.floor(Math.random()*3)];
     return choice;
 }
 
 function playRound(playerChoice, computerChoice) {
-    if ((playerChoice=="ROCK" && computerChoice=="Scissors") || (playerChoice=="PAPER" && computerChoice=="Rock") || (playerChoice=="SCISSORS" && computerChoice=="Paper")) {
-        console.log("You win!");
+    if ((playerChoice=='ROCK' && computerChoice=='Scissors') || (playerChoice=='PAPER' && computerChoice=='Rock') || (playerChoice=='SCISSORS' && computerChoice=='Paper')) {
+        let player = document.querySelector('.player-score');
+        player.innerHTML++;
+        playerScore++;
     }
     else {
-        console.log("You lose!");
+        let computer = document.querySelector('.computer-score');
+        computer.innerHTML++;
+        computerScore++;
     }
 }
 
-for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Enter rock, paper, or scrissors:");
-    let capitalizedChoice = playerChoice.toUpperCase();
-    let computerChoice = getComputerChoice();
-    playRound(capitalizedChoice, computerChoice);
+function clearScore() {
+    let player = document.querySelector('.player-score');
+    player.innerHTML = 0;
+    let computer = document.querySelector('.computer-score');
+    computer.innerHTML = 0;
 }
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () => {
+    playRound('ROCK', getComputerChoice());
+    if (playerScore==5 || computerScore==5) {
+        if (playerScore==5) {
+            alert('You won');
+        }
+        else if (computerScore==5) {
+            alert('You lose');
+        }
+        clearScore();
+    }
+});
+
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => {
+    playRound('PAPER', getComputerChoice());
+    if (playerScore==5 || computerScore==5) {
+        if (playerScore==5) {
+            alert('You won');
+        }
+        else if (computerScore==5) {
+            alert('You lose');
+        }
+        clearScore();
+    }
+});
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', () => {
+    playRound('SCISSORS', getComputerChoice());
+    if (playerScore==5 || computerScore==5) {
+        if (playerScore==5) {
+            alert('You won');
+        }
+        else if (computerScore==5) {
+            alert('You lose');
+        }
+        clearScore();
+    }
+});
